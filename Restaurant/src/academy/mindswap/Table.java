30199c0cb4fb7f564c12fe78f3432cfa2467ef8a
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-07-14 18:51:03 +0100

    Constructor, getter and setter on table class

--- Restaurant/src/academy/mindswap/Table.java
+++ Restaurant/src/academy/mindswap/Table.java
@@ -1,4 +1,24 @@
 package academy.mindswap;
 
 public class Table {
+
+    private boolean isFree;
+    private String order;
+
+    public Table() {
+        this.isFree = true;
+        this.order = "";
+    }
+    public boolean isFree() {
+        return isFree;
+    }
+    public void setFree(boolean free) {
+        isFree = free;
+    }
+    public String getOrder() {
+        return order;
+    }
+    public void setOrder(String order) {
+        this.order = order;
+    }
 }
